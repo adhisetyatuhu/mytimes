c37a14e731826a0d4c8e4447acb55c418fdc8879
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2025-02-03 11:59:34 +0700

    fix: warning on checkbox for  sidebar menu toggle

--- news-app/src/components/header.tsx
+++ news-app/src/components/header.tsx
@@ -78,6 +78,7 @@ export default function Header() {
                                 type="checkbox"
                                 className="drawer-toggle"
                                 checked={isDrawerOpen}
+                                onChange={() => setIsDrawerOpen(isDrawerOpen)}
                             />
                             <div className="drawer-content">
                                 {/* Page content here */}
